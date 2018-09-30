356743c8b63f445c744eb7a810c28e3d9aabdeaa
tips4kodi <tips4kodi@gmail.com>
2018-09-30 20:21:42 +0200

    103

--- plugin.program.t4kbuild/default.py
+++ plugin.program.t4kbuild/default.py
@@ -28,7 +28,7 @@ HOME         =  xbmc.translatePath('special://home/')
 
     
 def CATEGORIES():
-    link = OPEN_URL('https://pastebin.com/raw/MdrsgzZe').replace('\n','').replace('\r','')
+    link = OPEN_URL('https://pastebin.com/raw/c9eE5YMD').replace('\n','').replace('\r','')
     match = re.compile('name="(.+?)".+?rl="(.+?)".+?mg="(.+?)".+?anart="(.+?)".+?escription="(.+?)"').findall(link)
     for name,url,iconimage,fanart,description in match:
         addDir(name,url,1,iconimage,fanart,description)
