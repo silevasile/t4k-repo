4692004e838dbbdfded251be7bc0b5e376ef2faf
tips4kodi <mitzacyby@gmail.com>
2018-12-12 22:35:34 +0100

    updated t4k build

---
+++ plugin.program.t4kbuild/uservar.py
@@ -0,0 +1,116 @@
+import os, xbmc, xbmcaddon
+
+#########################################################
+### User Edit Variables #################################
+#########################################################
+ADDON_ID       = xbmcaddon.Addon().getAddonInfo('id')
+ADDONTITLE     = 'T4K Build Wizard'
+EXCLUDES       = [ADDON_ID]
+# Text File with build info in it.
+BUILDFILE      = 'https://rebrand.ly/1394a'
+# How often you would list it to check for build updates in days
+# 0 being every startup of kodi
+UPDATECHECK    = 0
+# Text File with apk info in it.
+APKFILE        = 'http://'
+# Text File with Youtube Videos urls.  Leave as 'http://' to ignore
+YOUTUBETITLE   = ''
+YOUTUBEFILE    = 'http://'
+# Text File for addon installer.  Leave as 'http://' to ignore
+ADDONFILE      = 'http://'
+# Text File for advanced settings.  Leave as 'http://' to ignore
+ADVANCEDFILE   = 'http://'
+
+# Dont need to edit just here for icons stored locally
+PATH           = xbmcaddon.Addon().getAddonInfo('path')
+ART            = os.path.join(PATH, 'resources', 'art')
+
+#########################################################
+### THEMING MENU ITEMS ##################################
+#########################################################
+# If you want to use locally stored icons the place them in the Resources/Art/
+# folder of the wizard then use os.path.join(ART, 'imagename.png')
+# do not place quotes around os.path.join
+# Example:  ICONMAINT     = os.path.join(ART, 'mainticon.png')
+#           ICONSETTINGS  = 'http://aftermathwizard.net/repo/wizard/settings.png'
+# Leave as http:// for default icon
+ICONBUILDS     = 'http://'
+ICONMAINT      = 'http://'
+ICONSPEED      = 'http://'
+ICONAPK        = 'http://'
+ICONRETRO      = 'http://'
+ICONADDONS     = 'http://'
+ICONYOUTUBE    = 'http://'
+ICONSAVE       = 'http://'
+ICONTRAKT      = 'http://'
+ICONREAL       = 'http://'
+ICONLOGIN      = 'http://'
+ICONCONTACT    = 'http://'
+ICONSETTINGS   = 'http://'
+# Hide the ====== seperators 'Yes' or 'No'
+HIDESPACERS    = 'No'
+# Character used in seperator
+SPACER         = '*'
+
+# You can edit these however you want, just make sure that you have a %s in each of the
+# THEME's so it grabs the text from the menu item
+COLOR1         = 'dodgerblue'
+COLOR2         = 'white'
+# Primary menu items   / %s is the menu item and is required
+THEME1         = '[COLOR '+COLOR1+'][B][COLOR '+COLOR2+'][/COLOR][/B][/COLOR] [COLOR '+COLOR2+']%s[/COLOR]'
+# Build Names          / %s is the menu item and is required
+THEME2         = '[COLOR '+COLOR2+']%s[/COLOR]'
+# Alternate items      / %s is the menu item and is required
+THEME3         = '[COLOR '+COLOR1+']%s[/COLOR]'
+# Current Build Header / %s is the menu item and is required
+THEME4         = '[COLOR '+COLOR1+']Current Build:[/COLOR] [COLOR '+COLOR2+']%s[/COLOR]'
+# Current Theme Header / %s is the menu item and is required
+THEME5         = '[COLOR '+COLOR1+']Current Theme:[/COLOR] [COLOR '+COLOR2+']%s[/COLOR]'
+
+# Message for Contact Page
+# Enable 'Contact' menu item 'Yes' hide or 'No' dont hide
+HIDECONTACT    = 'No'
+# You can add \n to do line breaks
+CONTACT        = 'Grup Facebook Kodi Romania - T4K'
+#Images used for the contact window.  http:// for default icon and fanart
+CONTACTICON    = 'http://'
+CONTACTFANART  = 'http://'
+#########################################################
+
+#########################################################
+### AUTO UPDATE #########################################
+########## FOR THOSE WITH NO REPO #######################
+# Enable Auto Update 'Yes' or 'No'
+AUTOUPDATE     = 'Yes'
+# Url to wizard version
+WIZARDFILE     = 'https://rebrand.ly/1394a'
+#########################################################
+
+#########################################################
+### AUTO INSTALL ########################################
+########## REPO IF NOT INSTALLED ########################
+# Enable Auto Install 'Yes' or 'No'
+AUTOINSTALL    = 'No'
+# Addon ID for the repository
+REPOID         = ''
+# Url to Addons.xml file in your repo folder(this is so we can get the latest version)
+REPOADDONXML   = ''
+# Url to folder zip is located in
+REPOZIPURL     = ''
+#########################################################
+
+#########################################################
+### NOTIFICATION WINDOW##################################
+#########################################################
+# Enable Notification screen Yes or No
+ENABLE         = 'Yes'
+# Url to notification file
+NOTIFICATION   = 'http://'
+# Use either 'Text' or 'Image'
+HEADERTYPE     = 'Text'
+HEADERMESSAGE  = ''
+# url to image if using Image 424x180
+HEADERIMAGE    = ''
+# Background for Notification Window
+BACKGROUND     = ''
+#########################################################
